bbfe7b0916a98fc0e1eafde9377fee853e2faf9f
cemujax <cemujax@gmail.com>
2019-02-16 11:54:51 +0900

    Added Routing Components

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Home
+    <div>Board List:
+      <ul>
+        <li>
+          <router-link to="/b/1">Board1</router-link>
+        </li>
+        <li>
+          <router-link to="/b/2">Board2</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
