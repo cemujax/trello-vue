d508351d1aaefa23e26fc131f384c4787036dc5e
cemujax <cemujax@gmail.com>
2019-02-24 20:05:19 +0900

    Implemented UPDATE_LIST

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="list" :data-list-id="data.id">
     <div class="list-header">
-      <div class="list-header-title">{{data.title}}</div>
+      <input
+        v-if="isEditTitle"
+        class="form-control"
+        type="text"
+        v-model="inputTitle"
+        ref="inputTitle"
+        @keyup.enter="onSubmitTitle"
+        @blur="onBlurTitle"
+      >
+      <div v-else class="list-header-title" @click="onClickTitle">{{data.title}}</div>
     </div>
     <div class="card-list">
       <CardItem v-for="card in data.cards" :key="card.id" :data="card"/>
@@ -18,14 +27,44 @@
 <script>
 import AddCard from "./AddCard.vue";
 import CardItem from "./CardItem.vue";
-
+import { mapActions } from "vuex";
 export default {
   components: { AddCard, CardItem },
   props: ["data"],
+  created() {
+    this.inputTitle = this.data.title;
+  },
   data() {
     return {
-      isAddCard: false
+      isAddCard: false,
+      isEditTitle: false,
+      inputTitle: ""
     };
+  },
+  methods: {
+    ...mapActions(["UPDATE_LIST"]),
+
+    onClickTitle() {
+      this.isEditTitle = true;
+      this.$nextTick(_ => this.$refs.inputTitle.focus());
+    },
+
+    onBlurTitle() {
+      this.isEditTitle = false;
+    },
+
+    onSubmitTitle() {
+      this.onBlurTitle();
+
+      this.inputTitle = this.inputTitle.trim();
+      if (!this.inputTitle) return;
+
+      const id = this.data.id;
+      const title = this.inputTitle;
+      if (title === this.data.title) return;
+
+      this.UPDATE_LIST({ id, title });
+    }
   }
 };
 </script>
